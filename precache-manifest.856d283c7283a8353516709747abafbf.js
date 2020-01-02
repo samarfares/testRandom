e5a381e8c469d4409e81541e3ae0f70288613a0a
samar fares <samarfares1991@gmail.com>
2020-01-02 12:59:15 +0300

    Updates

---
+++ precache-manifest.856d283c7283a8353516709747abafbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc3dd5e8382b4ccbd0e22b0d40b05d0",
+    "url": "/testRandom/index.html"
+  },
+  {
+    "revision": "7636d1c607ad4770be5d",
+    "url": "/testRandom/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "78a5949c1eb9cf90b85b",
+    "url": "/testRandom/static/js/2.c450ddeb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testRandom/static/js/2.c450ddeb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7636d1c607ad4770be5d",
+    "url": "/testRandom/static/js/main.db5fdc1b.chunk.js"
+  },
+  {
+    "revision": "c53e6fdfb76b09b49499",
+    "url": "/testRandom/static/js/runtime-main.6a0d922a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testRandom/static/media/logo.5d5d9eef.svg"
+  }
+]);
